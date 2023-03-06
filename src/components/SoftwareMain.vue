cde96d79412ed5334f95bdf3c6b55eb338c2ae71
p0wered <powered.ui@gmail.com>
2023-03-06 22:03:33 +0300

    working on software page

---
+++ src/components/SoftwareMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="software-tint"></div>
+  <section class="software-main" :style="{backgroundImage: softwareUrl}">
+    <div class="container">
+      <div class="software-text-box fb-column-center">
+        <div class="software-logo" :style="{backgroundImage: softwareImage}"></div>
+        <h1 class="homepage-title">{{softwareTitle}}</h1>
+        <p class="software-desc" style="margin: 0">{{softwareDesc}}</p>
+        <a href="#" class="download-btn">Download Now</a>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SoftwareMain",
+  props: ["softwareTitle", "softwareDesc", "softwareUrl", "softwareImage"]
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/SoftwarePage.vue
+++ src/pages/SoftwarePage.vue
@@ -1,10 +1,36 @@
 <template>
-
+  <div class="section-top"></div>
+  <div class="container">
+    <div class="hardware-title-block">
+      <h2 class="software-title">SOFTWARE</h2>
+      <p class="hardware-desc">Razer offers a wide range of gaming software to complete your Razer gaming experience,
+        whether it is allowing you to game lag-free, stream with customized Chroma effects, or amp up your audio for
+        competitive gaming advantage.
+      </p>
+    </div>
+  </div>
+  <template v-for="item in main" :key="item">
+    <SoftwareMain :software-title="item.softwareTitle" :software-desc="item.softwareDesc" :software-url="item.softwareUrl" :software-image="item.softwareImage"></SoftwareMain>
+  </template>
 </template>
 
 <script>
+import SoftwareMain from "@/components/SoftwareMain.vue";
+import axios from "axios";
+
 export default {
-  name: "SoftwarePage"
+  name: "SoftwarePage",
+  components: {SoftwareMain},
+  mounted() {
+    axios.get("backend/homepage/data.json").then(({data}) => {
+      this.main = data.software
+    })
+  },
+  data() {
+    return{
+      main: []
+    }
+  }
 }
 </script>
 
